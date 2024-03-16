659c97504ff68c212a50aaf9520f2b058bea9f05
Dima <probnie.blogi@gmail.com>
2024-03-16 12:48:07 +0200

    CreateBot

--- src/main/java/Main/AppLauncher.java
+++ src/main/java/Main/AppLauncher.java
@@ -1,7 +1,13 @@
 package Main;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import telegrambot.CurrencyExchangeBot;
+
 public class AppLauncher {
-    public static void main(String[] args) {
-        System.out.println("Project Go");
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new CurrencyExchangeBot());
     }
 }
