87faf4d357df2ec382605743be1218deb4188ebf
Dima <probnie.blogi@gmail.com>
2024-03-16 19:13:06 +0200

    Написав клієнт для моно і сам моно клас

--- src/main/java/bankModel/MonoBank.java
+++ src/main/java/bankModel/MonoBank.java
@@ -1,4 +1,15 @@
 package bankModel;
 
-public class MonoBank {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MonoBank{
+    int currencyCodeA;
+    int currencyCodeB;
+    long date;
+    float rateSell;
+    float rateBuy;
+    float rateCross;
 }
